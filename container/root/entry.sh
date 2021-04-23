fb29854ae8d8609634c557e78c1d5c90ad713d89
Anillc <an@kano.cat>
2021-04-24 02:52:14 +0800

    完善功能

--- container/root/entry.sh
+++ container/root/entry.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 if [ ! "$(ls -A /bird)" ]; then
-  cp /init.conf /bird/abird.conf
+  cp /abird.conf /bird/abird.conf
   mkdir /bird/dn42peers
   mkdir /bird/internetpeers
   mkdir /bird/intranetpeers
   exit 0
 fi
-mkdir -p /run/bird
-touch /run/bird/bird.ctl
-bird -f
+curl -sfSLR {-o,-z}/etc/bird/roa_dn42_v6.conf https://dn42.burble.com/roa/dn42_roa_bird2_6.conf
+curl -sfSLR {-o,-z}/etc/bird/roa_dn42.conf https://dn42.burble.com/roa/dn42_roa_bird2_4.conf
+/init
