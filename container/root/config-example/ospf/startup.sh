2422101535ec954642bded70c5d419d90cebbe4f
Anillc <i@anillc.cn>
2021-07-16 15:07:26 +0800

    增加宣告的 ip

--- container/root/config-example/ospf/startup.sh
+++ container/root/config-example/ospf/startup.sh
@@ -3,5 +3,5 @@ ip link del dummy2526
 ip link add dummy2526 type dummy
 ip addr add 172.22.167.97 dev dummy2526
 ip addr add fdc9:83c1:d0ce::1 dev dummy2526
-ip addr add 2a0e:b107:1170::1 dev dummy2526
+ip addr add 2602:feda:da0::1 dev dummy2526
 ip link set dummy2526 up
